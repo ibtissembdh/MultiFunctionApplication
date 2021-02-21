fd2dc46f33a9bf3a259b54090593e9c8f8f85ef6
ibtissembdh <ibtissem.bendouhou754@gmail.com>
2021-02-21 18:12:00 +0100

    send sms function using explicite Intent

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -2,13 +2,31 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity2 extends AppCompatActivity {
+    Button btn1, btn2, btn3, btn4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        btn1 = this.findViewById(R.id.btn1);
+        btn2 = this.findViewById(R.id.btn2);
+        btn3 = this.findViewById(R.id.btn3);
+        btn4 = this.findViewById(R.id.btn4);
+
+        btn1.setOnClickListener(v->SMS_Function());
+
+    }
+    public void SMS_Function(){
+        Uri uri = Uri.parse("sms:you are the best");
+        Intent intent= new Intent(Intent.ACTION_VIEW, uri);
+        intent.putExtra("sms_body","message");
+        startActivity(intent);
     }
 }
